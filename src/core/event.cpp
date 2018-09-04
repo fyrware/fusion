5785a126e74d0b0e7d828085af4ed10150e0d36a
Nick Dugger <email@nickdugger.com>
2018-09-04 14:28:32 -0500

    Update event.cpp

--- src/core/event.cpp
+++ src/core/event.cpp
@@ -4,4 +4,23 @@ using namespace std;
 
 namespace fusion::core {
 
+    template <class type> class event {
+        
+        private:
+            string name;
+            type target;
+        
+        public:
+            event (string name, type target) {
+                
+            }
+        
+            string get_name () {
+                return name;
+            }
+        
+            type get_target () {
+                return target;
+            }
+    }
 }
