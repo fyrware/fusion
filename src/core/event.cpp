20d951b0025adbd90e1914349919e9c469839e5d
Nick Dugger <email@nickdugger.com>
2018-09-04 22:57:17 -0500

    event

--- src/core/event.cpp
+++ src/core/event.cpp
@@ -1,3 +1,5 @@
+# include <any>
+# include <function>
 # include <map>
 # include <string>
 # include <vector>
@@ -18,29 +20,36 @@ namespace fusion::core {
 
             }
 
-            observable subscribe (string name) {
+            event::observable subscribe (string name) {
 
             }
         }
         
         static class observable {
             
-            vector<function> actions;
+            vector<map<string, function>> actions;
             
             observable& debounce (function callback) {
-                actions.push_back(callback);
+				actions.push_back({ { "debounce", callback } });
                 return this;
             }
             
             observable& filter (function callback) {
-                actions.push_back(callback);
+				actions.push_back({ { "filter", callback } });
                 return this;
             }
             
             observable& for_each (function callback) {
-                actions.push_back(callback);
+				actions.push_back({ { "for_each", callback } });
                 return this;
             }
+
+			void pipe (event e) {
+				for (int i = 0, int count = actions.size(); i < count; ++i) {
+					map<string, any> action = actions.at(i);
+					string name = action.
+				}
+			}
         }
         
         event (string name, map<string, any> details, target_type target) {
