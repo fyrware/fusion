0a40e431d1d6a92856e988dbd56894b158a175f2
Nick Dugger <email@nickdugger.com>
2018-09-07 03:49:21 -0500

    Update thread_pool.cpp

--- source/modules/thread_pool.cpp
+++ source/modules/thread_pool.cpp
@@ -1,6 +1,10 @@
+# include <fusion/core/module>
+
+using namespace fusion::core;
+
 namespace fusion::modules {
     
-    class thread_pool {
+    class thread_pool : public module {
         
     };
 }
