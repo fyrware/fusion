9ce2628d2b3536845aa421e441a4b407fbfa9705
Nick Dugger <email@nickdugger.com>
2018-12-05 21:44:02 -0600

    remove non-fusion code

--- example/main.cpp
+++ example/main.cpp
@@ -1,24 +1,14 @@
 # include <iostream>
+# include <map>
+# include <memory>
+# include <string>
 
-# include "fusion/program.cpp"
+# include "fusion/broker.cpp"
 
 namespace example {
 
     int run () {
-        fusion::program<int> example;
 
-        example.subscribe("foo").observe("bar").for_each([ & ] (fusion::instruction<int> x) {
-            example.exit(x.details());
-        });
-
-        example.subscribe("program").observe("start").for_each([ & ] (fusion::instruction<int> x) {
-            example.publish("foo", "bar", fusion::instruction<int>("foo::bar", 0));
-        });
-
-        example.start();
-
-        while (example.running());
-        return example.status();
     }
 }
 
